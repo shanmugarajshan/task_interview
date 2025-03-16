5bf63c0887b662e8076244ee96c30a4a57c9e921
Shan <shanmugarajshan10@gmail.com>
2025-03-16 22:39:00 +0530

    fix: again fix a error for build

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
-import Form from './taskOne/Form';
-import { Button } from 'reactstrap';
-import VendorTable from './taskOne/VendorTable';
-import CategoryForm from './taskTwo/Category';
+import { Route, Routes } from 'react-router-dom';
 import Sidebar from './TaskThree/Sidebar';
-import Dashboard from './TaskThree/DashBoard';
 import Invoice from './TaskThree/Invoice';
+import Dashboard from './TaskThree/DashBoard';
 
 function App() {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [activeTab, setActiveTab] = useState('/');
-
-  useEffect(() => {
-    setActiveTab(location.pathname);
-  }, [location.pathname]);
 
   return (
     <div>
